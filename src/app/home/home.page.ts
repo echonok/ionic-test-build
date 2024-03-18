a6ebbdb75ff976e8f8ce42f1d9c902e291cc7f12
Nick Elobsky <echonok@gmail.com>
2024-03-18 20:12:31 +0100

    test new

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { addOutline, closeOutline } from 'ionicons/icons';
+
+interface ICourse {
+  name: string;
+  rating: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  courseList: ICourse[] = [];
+  courseName: string = '';
+  courseRating: number = 0;
+  isAlertOpen = false;
+  alertButtons = ['Action'];
+
+  constructor() {
+    addIcons({ addOutline, closeOutline });
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
+
+  addExpense() {
+    if (this.courseName.trim().length < 0 || this.courseRating <= 0 || this.courseRating > 5) {
+      this.setOpen(true);
+      return;
+    }
+    this.courseList.push({ name: this.courseName, rating: this.courseRating });
+  }
+
+  clearInputs() {
+    this.courseName = '';
+    this.courseRating = 0;
+  }
 }
